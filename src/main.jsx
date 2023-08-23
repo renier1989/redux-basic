0bf094a6af9ebaa250be1d0ebf9d8c4de3aae39e
Renier Vargas <renier1989@gmail.com>
2023-08-23 17:05:20 -0500

    Class147 - Mas ejemplos de Redux

--- src/main.jsx
+++ src/main.jsx
@@ -6,13 +6,21 @@ import { createStore } from 'redux'
 // esto es un reducer y es el encargas de actualizar el estado de nuestra aplicacion
 const store = createStore((state= 0, action)=>{
   switch (action.type) {
-    case 'accion':{
-      return action.payload
+    case 'incrementar':{
+      return state + 1;
     }
+    case 'decrementar':{
+      return state - 1;
+    }
+    case 'set':{
+      return action.payload;
+    }
+    default:
+    return state
   }
   // action = {type: 'tipo de accion', payload: 'any'}
-  console.log({state, action});
-  return state;
+  // console.log({state, action});
+  // return state;
 })
 
 // el store tiene varias propiedades {getState() , dispatch()}
@@ -22,9 +30,17 @@ const store = createStore((state= 0, action)=>{
 // siempre que se llama al dispatch del store , este vuelve a ejecutar el reducer que es nuestro store 
 // siempre que se haga un didpatch este debe tener absolutamente siempre la propiedad de type , {type: '<nombre de la accion>'}
 // para pasar data se usa la propiedad payload y este debe llamarse estrictamente de esta forma. 
+// console.log(store.getState(), ' Antes de actualizar el state');
+// store.dispatch({type:'accion', payload: 2});
+// console.log(store.getState(), ' Despues de hacer un Dispatch y actualizar el state');
+
 console.log(store.getState(), ' Antes de actualizar el state');
-store.dispatch({type:'accion', payload: 2});
-console.log(store.getState(), ' Despues de hacer un Dispatch y actualizar el state');
+store.dispatch({type:'incrementar'});
+console.log(store.getState(), ' Aqui el valor del state debe ser 1');
+store.dispatch({type:'decrementar'});
+console.log(store.getState(), ' Aqui el valor del state debe ser 0');
+store.dispatch({type:'set', payload: 99});
+console.log(store.getState(), ' Aqui el valor del state debe ser 99');
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
