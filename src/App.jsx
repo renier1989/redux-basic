c018f43a24f3d64df4b28b79f110466507645f34
Renier Vargas <renier1989@gmail.com>
2023-08-24 10:52:10 -0500

    Class153 - Dividiendo la complejidad, combinando reducers

--- src/App.jsx
+++ src/App.jsx
@@ -1,84 +1,95 @@
 /* eslint-disable react/prop-types */
 import { useEffect, useRef, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
+import { combineReducers } from "redux";
 
 const initialState = {
   entities: [],
-  filter: 'all' , // complete || incomplete   ( estos seran los estados del filtor para mostrar los todos)
+  filter: "all", // complete || incomplete   ( estos seran los estados del filtor para mostrar los todos)
 };
-export const reducer = (state = initialState, action) => {
+
+export const filterReducer = (state = "all", action) => {
+  switch (action.type) {
+    case "filter/set":
+      return (state = action.payload);
+    default:
+      return state;
+  }
+};
+
+export const todoReducer = (state = [], action) => {
   switch (action.type) {
     case "todo/add": {
-      // console.log('aqui agrego un todo/');
-      // state.entities = [{}]
-      return {
-        ...state,
-        entities: state.entities.concat({ ...action.payload }),
-      }; // esto es para poder obtener y actualizar la lista de los TODOS con el formato correcto , xe: si tiene un id, un estado, contenido en forma de JSON por eso se usa en concat para concatenar con lo que ya estaba en el state de entities
+      return state.concat({ ...action.payload });
     }
     case "todo/complete": {
-      const newTodo = state.entities.map(todo => {
-        if(todo.id === action.payload.id){
-          return {...todo, completed: !todo.completed}
-        } 
-        return todo
-      })
-      return {
-        ...state,
-        entities : newTodo
-      }
+      const newTodo = state.map((todo) => {
+        if (todo.id === action.payload.id) {
+          return { ...todo, completed: !todo.completed };
+        }
+        return todo;
+      });
+      return newTodo;
     }
-    // aqui creao un action para setear el estado del filtro y saber que todos mostrar. 
-    case "filter/set": {
-      return {
-        ...state,
-        filter : action.payload
-      }
-    }
+    default:
+      return state;
   }
-  return state;
 };
 
+// esta es una version mucho mas simplificada para usar los reducers separados 
+export const reducer = combineReducers({
+  entities: todoReducer,
+  filter: filterReducer,
+})
 
-const TodoItem = ({todo}) => {
+// // aqui hago la separacion en reducers mas pequeños para poder separa complejidad
+// export const reducer = (state = initialState, action) => {
+//   return {
+//     entities: todoReducer(state.entities, action),
+//     filter: filterReducer(state.filter, action),
+//   };
+// };
+
+const TodoItem = ({ todo }) => {
   const dispatch = useDispatch();
-  return(
-    <li 
-    style={{ textDecoration: todo.completed ? 'line-through' : 'none' }}
-    onClick={()=>dispatch({type:'todo/complete', payload:todo})}
-    >{todo.title}</li>
-  )
-}
+  return (
+    <li
+      style={{ textDecoration: todo.completed ? "line-through" : "none" }}
+      onClick={() => dispatch({ type: "todo/complete", payload: todo })}
+    >
+      {todo.title}
+    </li>
+  );
+};
 
 // esta funcion lo que evalua es los estados del filter para filtrar los todo y retornarlos con el estado completed filtrado
-const selectedTodos = state => {
-  const {entities , filter} = state;
+const selectedTodos = (state) => {
+  const { entities, filter } = state;
 
-  if(filter === 'complete'){
-    return entities.filter(todo => todo.completed);
+  if (filter === "complete") {
+    return entities.filter((todo) => todo.completed);
   }
-  if(filter === 'incomplete'){
-    return entities.filter(todo => !todo.completed);
+  if (filter === "incomplete") {
+    return entities.filter((todo) => !todo.completed);
   }
-  return entities
-  
-}
+  return entities;
+};
 
 function App() {
   const input = useRef();
-  // const state = useSelector((x) => x); // aqui simplemente estaba retornado los valores completos de state. 
-  
+  // const state = useSelector((x) => x); // aqui simplemente estaba retornado los valores completos de state.
+
   // aqui le puedo pasar una funcion que me va retornar los todos dependiendo de los filtros
   // aqui cambio el nombre que la constante que traera los todos de "state" a "todos" para no generar confilcto con el state del reducer
-  const todos = useSelector(selectedTodos); 
-  
+  const todos = useSelector(selectedTodos);
+
   const dispatch = useDispatch();
   const [value, setValue] = useState("");
   // console.log(state, 'Rendering');
 
-  useEffect(()=>{
+  useEffect(() => {
     input.current.focus();
-  },[])
+  }, []);
 
   const submit = (e) => {
     e.preventDefault();
@@ -88,7 +99,7 @@ function App() {
     const id = Math.random().toString(36); // un id unico
     const todo = { title: value, completed: false, id: id }; // Creamos la estructura que va a tener el todos
     dispatch({ type: "todo/add", payload: todo }); // despachamos a accion para agreagar el todo al state
-    setValue(''); // limpiamos el values en el state
+    setValue(""); // limpiamos el values en el state
   };
   const handleChange = ({ target }) => {
     setValue(target.value);
@@ -100,11 +111,23 @@ function App() {
       <form onSubmit={submit}>
         <input ref={input} value={value} onChange={(e) => handleChange(e)} />
       </form>
-      <button onClick={()=>dispatch({type:'filter/set', payload:'all'})}>Mostrar All</button>
-      <button onClick={()=>dispatch({type:'filter/set', payload:'complete'})}>Completados</button>
-      <button onClick={()=>dispatch({type:'filter/set', payload:'incomplete'})}>Incompletos</button>
+      <button onClick={() => dispatch({ type: "filter/set", payload: "all" })}>
+        Mostrar All
+      </button>
+      <button
+        onClick={() => dispatch({ type: "filter/set", payload: "complete" })}
+      >
+        Completados
+      </button>
+      <button
+        onClick={() => dispatch({ type: "filter/set", payload: "incomplete" })}
+      >
+        Incompletos
+      </button>
       <ul>
-        {todos.map(todo => <TodoItem key={todo.id} todo={todo} />)}
+        {todos.map((todo) => (
+          <TodoItem key={todo.id} todo={todo} />
+        ))}
       </ul>
     </div>
   );
